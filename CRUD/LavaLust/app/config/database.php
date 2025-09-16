06734d2688bd7677e94b36c25183c16f2450566c
Jhansen Fabella <your_github_email@example.com>
2025-09-17 04:32:21 +0800

    Update database credentials to FreeSQLDatabase host

--- CRUD/LavaLust/app/config/database.php
+++ CRUD/LavaLust/app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12798906',
+    'password'	=> 'B36qqflvaU',
+    'database'	=> 'sql12798906',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
