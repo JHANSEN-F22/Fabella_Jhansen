b707c45fd4cdc6d85a5999b1e4023fa1fe8bfdcd
Jhansen Fabella <your_github_email@example.com>
2025-09-17 01:07:32 +0800

    Add composer.json, composer.lock, and vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fabella/crud-project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0c5f58c8f425980c4fa37132e34c9a3b8ed1c7c5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fabella/crud-project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0c5f58c8f425980c4fa37132e34c9a3b8ed1c7c5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
